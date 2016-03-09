bddcf8a0c39fd9ab6b121679b47cca842d53c461
christos <christos>
2016-03-09 19:47:13 +0000

    PR/50919: David Binderman: Re-do all the debug stuff in a more sustainable way.

--- libform/post.c
+++ libform/post.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: post.c,v 1.9 2003/03/09 00:57:19 lukem Exp $	*/
+/*	$NetBSD: post.c,v 1.10 2016/03/09 19:47:13 christos Exp $	*/
 
 /*-
  * Copyright (c) 1998-2000 Brett Lymn
@@ -63,11 +63,6 @@ post_form(FORM *form)
 		return E_NO_ROOM;
 	}
 
-#ifdef DEBUG
-	if (_formi_create_dbg_file() != E_OK)
-		return E_SYSTEM_ERROR;
-#endif
-	
 	form->in_init = 1;
 	if (form->form_init != NULL)
 		form->form_init(form);
