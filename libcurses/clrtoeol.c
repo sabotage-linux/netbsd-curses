784b80057303db62a9d3b6d01ca6195655dd2033
uwe <uwe@NetBSD.org>
2020-03-15 01:18:43 +0000

    werase, wclrtobot, wclrtoeol - make code even more similar. Rename attr to battr to match bch and WINDOW::battr. No functional change intended.

--- libcurses/clrtoeol.c
+++ libcurses/clrtoeol.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: clrtoeol.c,v 1.30 2020/03/13 02:57:26 roy Exp $	*/
+/*	$NetBSD: clrtoeol.c,v 1.31 2020/03/15 01:18:43 uwe Exp $	*/
 
 /*
  * Copyright (c) 1981, 1993, 1994
@@ -59,7 +59,17 @@ wclrtoeol(WINDOW *win)
 	int     minx, x, y;
 	__LDATA *end, *maxx, *sp;
 	wchar_t bch;
-	attr_t	attr;
+	attr_t	battr;
+
+#ifdef HAVE_WCHAR
+	bch = (wchar_t)btowc((int)win->bch);
+#else
+	bch = win->bch;
+#endif
+	if (win != curscr)
+		battr = win->battr & __ATTRIBUTES;
+	else
+		battr = 0;
 
 	y = win->cury;
 	x = win->curx;
@@ -76,24 +86,17 @@ wclrtoeol(WINDOW *win)
 	end = &win->alines[y]->line[win->maxx];
 	minx = -1;
 	maxx = &win->alines[y]->line[x];
-#ifdef HAVE_WCHAR
-	bch = (wchar_t)btowc((int)win->bch);
-#else
-	bch = win->bch;
-#endif
-	if (win != curscr)
-		attr = win->battr & __ATTRIBUTES;
-	else
-		attr = 0;
 
 	for (sp = maxx; sp < end; sp++) {
-		if (!(__NEED_ERASE(sp, bch, attr)))
+		if (!(__NEED_ERASE(sp, bch, battr)))
 			continue;
+
 		maxx = sp;
 		if (minx == -1)
 			minx = (int)(sp - win->alines[y]->line);
-		sp->attr = attr | (sp->attr & __ALTCHARSET);
+
 		sp->ch = bch;
+		sp->attr = battr | (sp->attr & __ALTCHARSET);
 #ifdef HAVE_WCHAR
 		if (_cursesi_copy_nsp(win->bnsp, sp) == ERR)
 			return ERR;
